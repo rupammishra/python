460bc8c9ce0140552921829f610975b7e9f4a8fc
Rupam Mishra <58908361+rupammishra@users.noreply.github.com>
2019-12-18 00:09:30 +0530

    Update text_to_speech.py
    Comments added

--- text_to_speech.py
+++ text_to_speech.py
@@ -1,11 +1,18 @@
+#library used to convert the text into speech
 from gtts import gTTS
+
+#library used to interact with the system
 import os
 
+#to open the text file 
 f = open('text.txt')
 x =f.read()
 
-language = 'bn'
+#language in which user want speech
+language = 'en'
+
 audio = gTTS(text=x, lang=language, slow=False)
 
+#saving the audio in mp3 format
 audio.save('speech.mp3')
 os.system('speech.mp3')
